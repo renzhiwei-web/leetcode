b9767b99d7e7b57eba4f84c1db80e1f891d56bd0
renzhiwei-web <renzhiwei565@163.com>
2020-11-24 16:15:36 +0800

    leetcode208题前缀树

---
+++ src/LeetCode208.java
@@ -0,0 +1,81 @@
+public class LeetCode208 {
+
+    private TrieNode root;
+
+    /** Initialize your data structure here. */
+    public LeetCode208() {
+        root = new TrieNode();
+    }
+
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+        TrieNode node = root;
+        for(int i = 0;i < word.length();i++){
+            char currentChar = word.charAt(i);
+            if (!node.containsKey(currentChar)){
+                node.put(currentChar,new TrieNode());
+            }
+            node = node.get(currentChar);
+        }
+        node.setEnd();
+    }
+
+    // search a prefix or whole key in trie and
+    // returns the node where search ends
+    private TrieNode searchPrefix(String word) {
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); i++) {
+            char curLetter = word.charAt(i);
+            if (node.containsKey(curLetter)) {
+                node = node.get(curLetter);//通过根节点找到子节点
+            } else {
+                return null;//否则，不是共同前缀
+            }
+        }
+        return node;
+    }
+
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        TrieNode node = searchPrefix(word);
+        return node != null && node.isEnd();
+    }
+
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix) {
+        TrieNode node = searchPrefix(prefix);
+        return node != null;
+    }
+}
+/*
+前缀树是一种多叉树,每个节点都存放着一个哈希表
+ */
+class TrieNode {
+
+    // R links to node children
+    private TrieNode[] links;
+
+    private final int R = 26;
+
+    private boolean isEnd;
+
+    public TrieNode() {
+        links = new TrieNode[R];
+    }
+
+    public boolean containsKey(char ch) {
+        return links[ch -'a'] != null;
+    }
+    public TrieNode get(char ch) {
+        return links[ch -'a'];
+    }
+    public void put(char ch, TrieNode node) {
+        links[ch -'a'] = node;
+    }
+    public void setEnd() {
+        isEnd = true;
+    }
+    public boolean isEnd() {
+        return isEnd;
+    }
+}
